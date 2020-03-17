0d622fba98aca6b5f5e14d3e36ddfa07bdaa257e
mechanic <tanviredu@yahoo.com>
2020-03-17 23:03:43 +0600

    added

--- guessinggame.sh
+++ guessinggame.sh
@@ -3,12 +3,20 @@
 checkans(){
 
     num=$1
-    re='^[0-9]+$'
-    if ! [[ $num =~ $re ]]; then
-        echo "enter a number please "
-    fi
-    return
+    re="^[0-9]+$"
+    if ! [[ $num =~ $re  ]] 2>/dev/null;
+    then
+        echo "please enter a number "
+        echo "you typed '${num}'  this is invalid"
+        
 
+    fi
+ ##       if ! [[ $num =~ $re ]]; then
+ ##           echo "enter a number please "
+ ##       fi
+
+    
+    
 }
 
 
@@ -16,8 +24,9 @@ checkans(){
 chalange(){
     echo "Guess how many files in the directory ?? "
     read -p " Enter your guess : " number
-
-    checkans $number 
+    
+    checkans $number
+    
 
 }
 
@@ -28,18 +37,23 @@ main(){
 
     ## we create a infinity loop and ask for a number
     while true;do
-    if [ $number -lt $ans  ]
+    if [[ $num =~ $re  ]] &&[ $number -lt $ans  ] 2>/dev/null 
     then
         echo "number is less than true ans"
         chalange
-    elif [ $number -gt $ans ]
+    elif [[ $num =~ $re  ]] && [ $number -gt $ans ] 2>/dev/null
     then
         echo "greater than the true ans"
         chalange
-    else
+    elif [[ $num -eq $ans ]] 2>/dev/null
+    then
         echo "you are right"
-        break;
+        break
+    else
+        echo "Something is wrong try again"
+        chalange
     fi
+
 
 done
  
